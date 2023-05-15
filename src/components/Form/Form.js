f2efc7550df515168ada4d1de56da28b20f6f4cd
Neshodym <voinash@gmail.com>
2023-05-15 16:35:27 +0900

    1

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -34,6 +34,7 @@ export const Form = ({ onSubmitProps }) => {
           id={nameId}
           type="text"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           className={css.formInput}
@@ -47,7 +48,8 @@ export const Form = ({ onSubmitProps }) => {
           value={phone}
           onChange={e => setPhone(e.target.value)}
           id={phoneId}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         ></input>
